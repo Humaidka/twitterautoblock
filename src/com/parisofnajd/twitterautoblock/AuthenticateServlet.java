43a5fe047a6de1b9bff9a0457126c75646b0e4ee
khaled <khaled@Jaguar>
2016-08-20 12:39:10 +0300

    .

--- src/com/parisofnajd/twitterautoblock/AuthenticateServlet.java
+++ src/com/parisofnajd/twitterautoblock/AuthenticateServlet.java
@@ -2,61 +2,37 @@ package com.parisofnajd.twitterautoblock;
 
 import java.io.*;
 import javax.servlet.http.*;
-import twitter4j.*;
-import twitter4j.auth.*;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 @SuppressWarnings("serial")
 public class AuthenticateServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		String authurl = null;
-		
-		try {
-			authurl = AuthenticateServlet.Authenticate();
-			resp.sendRedirect(authurl);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
-	private static String Authenticate() throws Exception{
-		// The factory instance is re-useable and thread safe.
-		Twitter twitter = TwitterFactory.getSingleton();
-		//twitter.setOAuthConsumer("[consumer key]", "[consumer secret]");
+	public void doGet(HttpServletRequest req,   HttpServletResponse resp) throws IOException {
+					String authurl;
+					try {
+						authurl = AuthenticateServlet.Authenticate(req);
+						resp.sendRedirect(authurl);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+	
+	private static  String Authenticate(HttpServletRequest req) throws Exception{
+		//ConfigurationBuilder cb = new ConfigurationBuilder();
+		//Configuration cfg = cb.build();
+		//TwitterFactory tf = new TwitterFactory(cfg);
+		Twitter twitter = new TwitterFactory().getInstance();
 		RequestToken requestToken = twitter.getOAuthRequestToken();
-		//AccessToken accessToken = null;
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//while (null == accessToken) {
-		//	System.out.println("Open the following URL and grant access to your account:");
+			HttpSession session = req.getSession();
+			session.setAttribute("request_token", requestToken);
 		    return requestToken.getAuthorizationURL();
-		  //  System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
-		  //  String pin = br.readLine();
-		  //  try{
-		  //     if(pin.length() > 0){
-		  //       accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-		  //     }else{
-		   // 	 accessToken = twitter.getOAuthAccessToken();
-		   //    }
-		  //    } catch (TwitterException te) {
-		   //     if(401 == te.getStatusCode()){
-	//	          System.out.println("Unable to get the access token.");
-		 //       }else{
-		 //         te.printStackTrace();
-		   //     }
-		 //     }
-		   // }
-		    //persist to the accessToken for future reference.
-		    //storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
-		    //String[] args = null;
-			//Status status = twitter.updateStatus(args[0]);
-		    //System.out.println("Successfully updated the status to [" + status.getText() + "].");
-		    //System.exit(0);
 		  }
-		  //private static void storeAccessToken(long useId, AccessToken accessToken){
-		    //store accessToken.getToken()
-		    //store accessToken.getTokenSecret()
-			//}
+
 	
 	
 	
--- src/com/parisofnajd/twitterautoblock/AutoBlockServlet.java
+++ src/com/parisofnajd/twitterautoblock/AutoBlockServlet.java
@@ -2,41 +2,51 @@ package com.parisofnajd.twitterautoblock;
 
 import java.io.*;
 import javax.servlet.http.*;
-import twitter4j.*;
-import twitter4j.auth.*;
+import java.util.logging.Logger;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
 
 @SuppressWarnings("serial")
 public class AutoBlockServlet extends HttpServlet {
+	private static final Logger log = Logger.getLogger(AutoBlockServlet.class.getName());
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		// The factory instance is re-useable and thread safe.
-		Twitter twitter = TwitterFactory.getSingleton();
-		AccessToken accessToken = null;
-		try {
-			accessToken = twitter.getOAuthAccessToken();
-			resp.getWriter().println(accessToken.getUserId());
+		 Twitter twitter = new TwitterFactory().getInstance();
+		 HttpSession session = req.getSession();
+		 AccessToken accessToken = null;
+		 if ( session.getAttribute("access_token") == null)
+			{
+			 log.info("Twitter Auto Block: access_token is null");
+			 RequestToken requestToken = (RequestToken) session.getAttribute("request_token");
+			 log.info("request_token: "+requestToken.toString());
+		 try { 
+		        	   accessToken = twitter.getOAuthAccessToken(requestToken,req.getParameter("oauth_verifier"));
+		        	   log.info("Twitter Auto Block: request_token: "+requestToken.toString());
+		        	   resp.getWriter().println(accessToken.toString());
+		        	   session.setAttribute("access_token", accessToken);
+		 } catch (TwitterException e) {
+					e.printStackTrace();
 		}
-		catch (TwitterException te) {
-			if(401 == te.getStatusCode()){
-			System.out.println("Unable to get the access token.");
-			}else{
-		    te.printStackTrace();
-		   }
-			
-			
-			
-			
-		     }
+			}
+		 else {
+		 accessToken = (AccessToken) session.getAttribute("access_token");
+		 }
+		 log.info("Twitter Auto Block: access_token: "+accessToken.toString());
+		 Query query = new Query("%40GaryLineker");
+		    try {
+				QueryResult result = twitter.search(query);
+				resp.getWriter().println(result.toString());
+			} catch (TwitterException e) {
+				// TODO Auto-generated catch block
+				resp.getWriter().println(e.toString());
+			}
 		    }
-		    //persist to the accessToken for future reference.
-		    //storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
-		    //String[] args = null;
-			//Status status = twitter.updateStatus(args[0]);
-		    //System.out.println("Successfully updated the status to [" + status.getText() + "].");
-		    //System.exit(0);
-			//private static void storeAccessToken(long useId, AccessToken accessToken){
-    		//store accessToken.getToken()
-    		//store accessToken.getTokenSecret()
-			//}
 			}
 		  
 	
